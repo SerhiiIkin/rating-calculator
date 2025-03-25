bcd79bc29c12d6b4eea8cf8d0490a78144a9052b
Serhii <messileonl@gmail.com>
2025-03-25 11:11:22 +0100

    add url

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         tailwindcss(),
         compression(),
         sitemap({
-            hostname: "https://yourdomain.com",
+            hostname: "https://rating-calculator-sable.vercel.app",
         }),
     ],
 });
